8a18b795816c3ab3f9b0a214127d0c61e65efcc2
Anirudh Garg <121122171+anuamloh07@users.noreply.github.com>
2023-09-05 22:45:07 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-home.addEventListener('mouseover', function() {
-    var containers = document.querySelectorAll('.container');
-    for (var i = 0; i < containers.length; i++) {
-      containers[i].style.color = "blue";
-    }
-  });
-  
