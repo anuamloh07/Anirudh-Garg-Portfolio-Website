b4f4de6021b0df84b90484f86b9d0219044b59f2
Anirudh Garg <121122171+anuamloh07@users.noreply.github.com>
2023-09-05 22:38:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById('demo-stocky').onclick=func1;
+function func1(){
+    window.location.assign('https://stocky007.netlify.app/');
+}
+
+document.getElementById('demo-converto').onclick=func2;
+function func2(){
+    window.location.assign('https://anuamloh07.github.io/converto/');
+}
+
+document.getElementById('demo-todo').onclick=func3;
+function func3(){
+    window.location.assign('');
+}
+
+document.getElementById('demo-g7fitness').onclick=func4;
+function func4(){
+    window.location.assign('https://g7fitness.netlify.app/');
+}
+
+document.getElementById('github-stocky').onclick=func5;
+function func5(){
+    window.location.assign('https://github.com/anuamloh07/Stocky');
+}
+
+document.getElementById('github-converto').onclick=func6;
+function func6(){
+    window.location.assign('https://github.com/anuamloh07/converto');
+}
+
+document.getElementById('github-todo').onclick=func7;
+function func7(){
+    window.location.assign('https://github.com/anuamloh07/flutter-app');
+}
+
+document.getElementById('github-g7fitness').onclick=func8;
+function func8(){
+    window.location.assign('https://github.com/anuamloh07/XYZ');
+}
+
+
+
+
+
